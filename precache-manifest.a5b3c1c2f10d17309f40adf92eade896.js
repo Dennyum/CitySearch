7ae72e17fcf703597dff2c1f466dad338e4a8b43
unknown <fernandobp_25@hotmail.com>
2020-04-01 22:36:30 +0200

    Updates

---
+++ precache-manifest.a5b3c1c2f10d17309f40adf92eade896.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcf60aebed0da0a463d9c3619d59b669",
+    "url": "/.github.io/CitySearch/index.html"
+  },
+  {
+    "revision": "c38d5d4632edcd015cd1",
+    "url": "/.github.io/CitySearch/static/css/main.cca9d42f.chunk.css"
+  },
+  {
+    "revision": "2cf0b376a15e1ba019eb",
+    "url": "/.github.io/CitySearch/static/js/2.640889b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/.github.io/CitySearch/static/js/2.640889b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c38d5d4632edcd015cd1",
+    "url": "/.github.io/CitySearch/static/js/main.0379a51c.chunk.js"
+  },
+  {
+    "revision": "06b16b7984a2864bcc5c",
+    "url": "/.github.io/CitySearch/static/js/runtime-main.687500f4.js"
+  },
+  {
+    "revision": "f5fc431435bda369e4962abf34133eb0",
+    "url": "/.github.io/CitySearch/static/media/foto4.f5fc4314.jpg"
+  }
+]);
